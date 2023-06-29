aec98cf12bf34b113eb8846b6d72c5f707dfcf4f
Angry-Creator <nwankwochidera252565@gmail.com>
2023-06-29 17:19:02 +0100

    Old site

--- v9/js/verification.js
+++ v9/js/verification.js
@@ -15,7 +15,7 @@ residentVerficationSelection.addEventListener("change", () => {
         idMePassword.style.display = "none";
         mortgageLetter.style.display = "none";
         utilityBill.style.display = "block";
-    } else if(selectedValue == "Mortgage Letter"){
+    } else if (selectedValue == "Mortgage Letter") {
         idMeEmail.style.display = "none";
         idMePassword.style.display = "none";
         utilityBill.style.display = "none";
@@ -26,8 +26,55 @@ residentVerficationSelection.addEventListener("change", () => {
 
 //Submition of the form
 const all_forms = document.getElementsByTagName("form");
-for(let i = 0; i < 2; i++){
-    const formData = new FormData(all_forms[i]);
-    const formValues = Object.fromEntries(formData.entries());
-    console.log(formValues);
+for (let i = 0; i < 2; i++) {
+    // console.log(formValues);
+}
+
+// const fileReader = new FileReader();
+// fileReader.onload = function () {
+//     const fileData = fileReader.result;
+//     const modifiedFile = new Blob([fileData], { type: fileInput.type });
+
+//     // Create a new file with the modified name
+//     const modifiedFileWithNewName = new File([modifiedFile], newFileName, { type: fileInput.type });
+
+//     console.log(modifiedFileWithNewName);
+
+//     // You can now perform operations with the modified file, such as uploading it to a server or saving it locally.
+// };
+
+// fileReader.readAsArrayBuffer(fileInput);
+
+
+// fetch('/upload', {
+//     method: 'POST',
+//     body: formData
+// })
+//     .then(response => response.text())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+//["template-application-driver-license-back"]
+//["template-application-driver-license-front"]
+//["template-application-mortgage-letter"]
+//["template-application-utility-bill"]
+
+
+
+const formData = new FormData(all_forms[1]);
+const formValues = Object.fromEntries(formData.entries());
+const driverLicenseBack = formData.get("template-application-driver-license-back");
+const driverLicenseFront = formData.get("template-application-driver-license-front");
+const mortagegeLetter = formData.get("template-application-mortgage-letter");
+const utilityBill = formData.get("template-application-utility-bill");
+console.log(!driverLicenseBack);
+console.log(!driverLicenseFront);
+console.log(!mortagegeLetter);
+console.log(!utilityBill);
+
+const submitBtn = document.getElementById("btn2");
+all_forms[1].addEventListener("submit", () => submitDetails());
+
+function submitDetails() {
+    alert("Done!");
 }
